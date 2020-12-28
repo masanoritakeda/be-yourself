41169277939a6f443b9fb1c3c96f5d14ab3cb315
masanori.takeda <t.masanori0712@icloud.com>
2020-12-28 04:04:00 +0000

    途中

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show, :edit,:update]
+  
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+     name: params[:name],
+     email: params[:email],
+     image: "a.png" 
+      )
+    
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "ユーザー登録が完了しました。"
+      redirect_to @user
+    else
+      render :new
+    end
+  end 
+    
+  def edit
+   @user = User.find(params[:id])
+   
+  end 
+    
+  def update 
+    @user = User.find(params[:id])
+      
+    if current_user == @user
+      if @user.update(user_params)
+        flash[:success] = 'ユーザー情報を編集しました。'
+        render :edit
+      else 
+        flash.now[:damger] = 'ユーザー情報の編集に失敗しました。'
+        render :edit
+      end 
+    else 
+      redirect_to root_url
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :introduce, :image)
+  end 
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+ root to: 'toppages#index'
+ 
+ get 'login', to: 'sessions#new'
+ post 'login', to: 'sessions#create'
+ delete 'logout', to: 'sessions#destroy'
+ get 'signup', to: 'users#new'
+ 
+ resources :users, only: [:show, :create, :edit, :update] do
+   member do
+     post 'edit'
+   end 
+ end 
+end
