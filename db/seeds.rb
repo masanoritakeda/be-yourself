a89395c0e35a31c7f32d9512f4eee3ff41d5cf3e
masanori.takeda <t.masanori0712@icloud.com>
2021-04-09 00:32:50 +0000

    確認

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 Category.create!([
-  {category_id: 1, name: "海・川"},
-  {category_id: 2, name: "自然"},
-  {category_id: 3, name: "観光"},
-  {category_id: 4, name: "日常"},
-  {category_id: 5, name: "食べ物"},
-  {category_id: 6, name: "景色"},
-  {category_id: 7, name: "イベント"},
-  {category_id: 8, name: "街並み"},
-  {category_id: 9, name: "休日"},
-  {category_id: 10, name: "趣味"},
-  {category_id: 11, name: "運動"},
-  {category_id: 12, name: "その他"}
+  {id: 1, name: "海・川"},
+  {id: 2, name: "自然"},
+  {id: 3, name: "観光"},
+  {id: 4, name: "日常"},
+  {id: 5, name: "食べ物"},
+  {id: 6, name: "景色"},
+  {id: 7, name: "イベント"},
+  {id: 8, name: "街並み"},
+  {id: 9, name: "休日"},
+  {id: 10, name: "趣味"},
+  {id: 11, name: "運動"},
+  {id: 12, name: "その他"}
 ])
 Post.create!([
   {content: "テスト", title: "練習です", image: "something.jpg", place: "江ノ島", user_id: 1}
