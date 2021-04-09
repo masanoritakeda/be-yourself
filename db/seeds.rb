d13e2fa11db6cc5039cfa84637d4220b89edcd6b
masanori.takeda <t.masanori0712@icloud.com>
2021-04-09 00:37:27 +0000

    確認

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,3 @@ Category.create!([
   {id: 11, name: "運動"},
   {id: 12, name: "その他"}
 ])
-Post.create!([
-  {content: "テスト", title: "練習です", image: "something.jpg", place: "江ノ島", user_id: 1}
-])
-PostCategory.create!([
-  {post_id: 1, category_id: 2},
-  {post_id: 1, category_id: 3}
-])
-User.create!([
-  {email: "t.masanori0712@icloud.com", password_digest: "$2a$12$MqXqRXckdzlVDUCpLJENV.rtaPcMVh5n2J/dc9mnS6JqHU4MW1GH.", introduce: nil, image: "IMG_2436.JPG", name: "masa takeda"}
-])
